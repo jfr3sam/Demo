e835b01d8de8d310faacb50ec45f70b734d1dc19
jfr3sam <jfr3sam@gmail.com>
2023-10-18 22:08:24 +0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# Demo
+# Demo Project
+
+This README provides step-by-step instructions for setting up a Python virtual environment across different operating systems. The guide also includes additional tips for working with Flask and SQLite.
+
+## Table of Contents
+
+1. [Setting Up a Virtual Environment](#setting-up-a-virtual-environment)
+2. [Activating the Virtual Environment](#activating-the-virtual-environment)
+3. [Working with Flask](#working-with-flask)
+4. [Working with SQLite](#working-with-sqlite)
+
+## Setting Up a Virtual Environment
+
+### Windows
+
+1. **Open Command Prompt**: Press `Windows + R`, type `cmd`, and hit Enter.
+2. **Navigate to Project Directory**: Use the `cd` command to navigate to your project folder.
+3. **Create Virtual Environment**:  
+    ```cmd
+    python -m venv myenv
+    ```
+
+### macOS and Linux
+
+1. **Open Terminal**: Open it from the Applications folder or search for it using Spotlight (`Cmd + Space`).
+2. **Navigate to Project Directory**: Use the `cd` command to go to your project folder.
+3. **Create Virtual Environment**:  
+    ```bash
+    python3 -m venv myenv
+    ```
+
+## Activating the Virtual Environment
+
+### Windows
+
+1. **Activate Virtual Environment**:  
+    ```cmd
+    .\myenv\Scripts\Activate
+    ```
+
+### macOS and Linux
+
+1. **Activate Virtual Environment**:  
+    ```bash
+    source myenv/bin/activate
+    ```
+
+## Working with Flask
+
+1. **Install Flask**:  
+    ```bash
+    pip install Flask
+    ```
+2. **Run Flask Application**:  
+    ```bash
+    flask run
+    ```
+
+## Working with SQLite
+
+1. **Install SQLite**:  
+    ```bash
+    pip install sqlite
+    ```
+2. **Initialize Database**:  
+    ```bash
+    python init_db.py
+    ```
+
--- src/app.py
+++ src/app.py
@@ -3,32 +3,61 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 def setup_database():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)')
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)')
+
+    sample_users = [
+        ('Ali', 'password1'),
+        ('Ahammed', 'password2'),
+        ('Mohammed', 'password3'),
+        ('esam', 'password4'),
+        ('empty', '')  # User with an empty password
+    ]
+
+    cursor.executemany(
+        'INSERT INTO users (username, password) VALUES (?, ?)', sample_users)
+
     conn.commit()
     conn.close()
+
+
+# Call setup_database function before running the app
+setup_database()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
+
+        # # Debugging lines to print the SQL query
+        # sql_query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+        # print("Executing SQL query:", sql_query)
+
         # Unsafe SQL query (Vulnerable to SQL Injection)
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
-        cursor.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'")
+        cursor.execute(
+            f"SELECT * FROM users WHERE username='{username}' AND password='{password}'")
+
         result = cursor.fetchone()
+
+        # # Debugging line to print the result
+        # print("Query result:", result)
         conn.close()
-        
+
         if result:
-            return "Logged in successfully (But beware, this is insecure!)"
+            return "Logged in successfully"
         else:
             return "Invalid credentials"
-        
+
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     setup_database()
